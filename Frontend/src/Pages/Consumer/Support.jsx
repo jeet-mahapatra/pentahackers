f72f4f73bd14f3e110542709fde8e09083e21ef3
Sayan2713 <sayanmondal619@gmail.com>
2026-05-17 11:36:11 +0530

    Fix modal filename casing

--- Frontend/src/Pages/Consumer/Support.jsx
+++ Frontend/src/Pages/Consumer/Support.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { Search, X } from 'lucide-react';
 import ContactChannels from './ContactChannels';
 import FAQ from './FAQ';
-import conciergeModal from './conciergeModal';
+import ConciergeModal from "./ConciergeModal";
 import { UserContext } from "../../Context/UserContext";
 
 const Support = () => {
